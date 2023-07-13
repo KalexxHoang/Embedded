84a395d0df818bea37c38d099b5baf11cd1d6974
KalexxHoang <hoangvanlam.iq2000@gmail.com>
2023-07-13 17:32:20 +0700

    13/7/2023

---
+++ 8051/Intro8051/main.c
@@ -0,0 +1,20 @@
+#include <regx52.h>
+
+void main()
+{
+		// Initialize
+		P2_0 = 0;
+		P2_1 = 1;
+		P2_7 = 0;
+	
+		P1 = 5;
+	/*	P1 7 6 5 4 3 2 1 0
+			5	 0 0 0 0 0 1 0 1
+	*/
+	
+		while(1)
+		{
+		// Operating program
+			
+		}
+}
